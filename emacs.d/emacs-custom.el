1fa7649651e94ea9019a8e90f6656f41e160715c
Martin Sosic <sosic.martin@gmail.com>
2012-10-12 22:17:06 +0200

    Added emacs dotfiles.

---
+++ emacs.d/emacs-custom.el
@@ -0,0 +1,35 @@
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(column-number-mode t)
+ '(custom-enabled-themes (quote (tango-dark)))
+ '(fancy-splash-image nil)
+ '(font-use-system-font t)
+ '(speedbar-show-unknown-files t)
+ '(sr-speedbar-right-side nil)
+ '(wg-display-current-workgroup-left-decor "")
+ '(wg-display-current-workgroup-right-decor "")
+ '(wg-mode-line-divider "|")
+ '(wg-mode-line-left-brace "wg:[")
+ '(wg-mode-line-on t)
+ '(wg-mode-line-right-brace "]")
+ '(wg-query-for-save-on-emacs-exit nil)
+ '(wg-restore-position t))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal))))
+ '(wg-brace-face ((((class color)) (:foreground "purple"))))
+ '(wg-command-face ((((class color)) (:foreground "blue"))))
+ '(wg-current-workgroup-face ((((class color)) (:foreground "red" :box (:line-width 2 :color "red")))))
+ '(wg-divider-face ((((class color)) (:foreground "purple"))))
+ '(wg-filename-face ((((class color)) (:foreground "blue"))))
+ '(wg-frame-face ((((class color)) (:foreground "blue"))))
+ '(wg-message-face ((((class color)) (:foreground "blue"))))
+ '(wg-mode-line-face ((((class color)) (:foreground "blue"))))
+ '(wg-other-workgroup-face ((((class color)) (:foreground "blue"))))
+ '(wg-previous-workgroup-face ((((class color)) (:foreground "blue")))))
---
+++ emacs.d/emacs.el
@@ -0,0 +1,27 @@
+(setq custom-file (concat user-emacs-directory ".emacs-custom"))
+(load custom-file)
+
+(add-to-list 'load-path (concat user-emacs-directory "elisp/"))
+(require 'sr-speedbar)
+(require 'workgroups "workgroups/workgroups")
+
+;;; open emacs in fullscreen
+(defun fullscreen () "fullscreen"
+  (interactive)
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+     '(2 "_NET_WM_STATE_MAXIMIZED_VERT" 0))
+  (x-send-client-message nil 0 nil "_NET_WM_STATE" 32
+     '(2 "_NET_WM_STATE_MAXIMIZED_HORZ" 0))
+)
+;(fullscreen)
+
+;;; tree browser
+;(sr-speedbar-open)
+
+;;; minor modes
+(column-number-mode t) ; column number is shown at mode line
+
+;;; windows layout
+(workgroups-mode 1)
+(wg-load (concat user-emacs-directory "myWorkgroups"))
+(add-hook 'kill-emacs-hook (lambda () (wg-save wg-file)))
