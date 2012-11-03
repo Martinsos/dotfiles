e22321a0d7312400a2278f05d0b3983b509fa639
Martin Sosic <sosic.martin@gmail.com>
2012-11-03 18:44:02 +0100

    Customized emacs to put all backup files in same directory.

--- emacs.d/emacs-custom.el
+++ emacs.d/emacs-custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(backup-directory-alist (quote ((".*" . "~/emacs_backup"))))
  '(c-basic-offset 4)
  '(column-number-mode t)
  '(custom-enabled-themes (quote (tango-dark)))
