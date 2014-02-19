57b5959c3facf0aabaf61765614ebeec503a2dc8
Martin Sosic <sosic.martin@gmail.com>
2014-02-19 16:24:26 +0100

    Added bash files

--- install.sh
+++ install.sh
@@ -4,16 +4,25 @@
 
 DOT_DIR=$PWD/$(dirname $0) # absolute path to dotfiles directory
 
-# Takes two params, source and target, and creates link if it does not exist
+# Takes source and target and creates link if it does not exist
 install_link() {
     local s=$1
     local t=$2
 
-    # if correct link exists
+    # if correct link exists do nothing
     [[ -L $t && $(readlink -f $t) == $(readlink -f $s) ]] && return 0
 
-    ln --s --no-target-directory --interactive --verbose $s $t
+    ln --symbolic --no-target-directory --interactive --verbose $s $t
 }
 
 # emacs
 install_link $DOT_DIR/emacs.d $HOME/.emacs.d
+
+# bash
+install_link $DOT_DIR/bashrc $HOME/.bashrc
+install_link $DOT_DIR/bash_aliases $HOME/.bash_aliases
+if [ ! -e $HOME/.bash_local ]; then 
+	echo "# Put settings specific for this machine here. Loaded by .bashrc" \
+		> $HOME/.bash_local
+	echo "$HOME/.bash_local created" 
+fi
