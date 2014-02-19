d9dfc3a9d07b1c680efe6df35c68cb194924b5b9
Martin Sosic <sosic.martin@gmail.com>
2014-02-19 15:10:14 +0100

    Added comments

--- install.sh
+++ install.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-DOT_DIR=$PWD/$(dirname $0)
+# This script assumes it is positioned in dotfiles directory
+
+DOT_DIR=$PWD/$(dirname $0) # absolute path to dotfiles directory
 
 # Takes two params, source and target, and creates link if it does not exist
 install_link() {
