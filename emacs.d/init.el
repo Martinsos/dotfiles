3dc4007f7e6a470c0803d7edf849dd3632ee95a6
Martin Sosic <sosic.martin@gmail.com>
2014-05-28 12:48:47 +0200

    Fixed errors that occur in terminal mode

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -44,8 +44,11 @@
 (windmove-default-keybindings 'meta) ; change buffer with M+arrow
 
 ;;; minor modes
-(tool-bar-mode -1) ; remove tool bar
-(scroll-bar-mode -1) ; remove scrolls
+(if (display-graphic-p)
+  (progn
+    (tool-bar-mode -1) ; remove tool bar
+    (scroll-bar-mode -1))) ; remove scrolls
+
 (column-number-mode t) ; column number is shown at mode line
 (global-linum-mode t) ; show line numbers
 (show-paren-mode t) ; highlight matching parent
