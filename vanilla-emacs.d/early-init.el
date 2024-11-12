9932457e97b97881062ddc4b1c8707c9a941283b
Martin Sosic <sosic.martin@gmail.com>
2024-11-12 09:15:12 +0100

    vanilla emacs -> minor stuff.

--- vanilla-emacs.d/early-init.el
+++ vanilla-emacs.d/early-init.el
@@ -1,3 +1,3 @@
-;; NOTE: This file was generated from Emacs.org on 2024-11-04 22:38:39 CET, don't edit it manually.
+;; NOTE: This file was generated from Emacs.org on 2024-11-12 09:14:06 CET, don't edit it manually.
 
 (setq package-enable-at-startup nil) ; Elpaca requires this.
