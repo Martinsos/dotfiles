930e522b36704286fa15cc698251143d50340f73
Martin Sosic <sosic.martin@gmail.com>
2025-04-04 22:47:06 +0200

    Small improvements (agenda, ...)

--- vanilla-emacs.d/early-init.el
+++ vanilla-emacs.d/early-init.el
@@ -1,4 +1,4 @@
-;; NOTE: This file was generated from Emacs.org on 2025-04-01 11:14:07 CEST, don't edit it manually.
+;; NOTE: This file was generated from Emacs.org on 2025-04-04 22:45:21 CEST, don't edit it manually.
 
 (setq package-enable-at-startup nil) ; Elpaca requires this.
 
