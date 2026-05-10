eacfa1e730d613909b4412272fe91ed398ae2241
Martin Sosic <sosic.martin@gmail.com>
2026-05-10 15:57:55 +0200

    Updated elpaca package updating instructions.

--- vanilla-emacs.d/init.el
+++ vanilla-emacs.d/init.el
@@ -1,6 +1,6 @@
 ;;; -*- lexical-binding: t; -*-
 
-;; NOTE: This file was generated from Emacs.org on 2026-05-10 15:02:30 CEST, don't edit it manually.
+;; NOTE: This file was generated from Emacs.org on 2026-05-10 15:57:21 CEST, don't edit it manually.
 
 (defvar elpaca-installer-version 0.11)
 (defvar elpaca-directory (expand-file-name "elpaca/" user-emacs-directory))
