41cebc88e22572f1011e3eecd244c3d50cdd999e
Martin Sosic <sosic.martin@gmail.com>
2026-05-11 15:20:16 +0200

    Tiny addition to my instructions.

--- vanilla-emacs.d/early-init.el
+++ vanilla-emacs.d/early-init.el
@@ -1,4 +1,4 @@
-;; NOTE: This file was generated from Emacs.org on 2026-05-10 22:17:03 CEST, don't edit it manually.
+;; NOTE: This file was generated from Emacs.org on 2026-05-11 15:20:03 CEST, don't edit it manually.
 
 (setq package-enable-at-startup nil) ; Elpaca requires this.
 
