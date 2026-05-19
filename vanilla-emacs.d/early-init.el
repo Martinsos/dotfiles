8596a9a3f3968fa2aef52d520c56f08d8ebb44da
Martin Sosic <sosic.martin@gmail.com>
2026-05-19 22:44:58 +0200

    Updated org-present.

--- vanilla-emacs.d/early-init.el
+++ vanilla-emacs.d/early-init.el
@@ -1,4 +1,4 @@
-;; NOTE: This file was generated from Emacs.org on 2026-05-19 21:44:44 CEST, don't edit it manually.
+;; NOTE: This file was generated from Emacs.org on 2026-05-19 22:44:10 CEST, don't edit it manually.
 
 
 
